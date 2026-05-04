66e650382cde06ac22c4604d4b0c35d6887d2df2
Sasindu Alahakoon <dilsharasasindu@gmail.com>
2026-05-04 15:07:29 +0530

    Remove build directory

--- wso2-integrator-docs/build/assets/js/aba21aa0.d8c027ac.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkwso2_integrator_docs=globalThis.webpackChunkwso2_integrator_docs||[]).push([[5742],{7093(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
